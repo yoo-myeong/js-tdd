c4ae56d57fbf676dc1b3776006c69434c1eace13
yoo-myeong <yoou201@naver.com>
2022-03-03 23:45:16 +0900

    mock 파일 제외하고 전부 basic폴더로 이동, mock을 사용한 콜백함수 테스트 코드

---
+++ unit_test/src/mock/check/test/check.test.js
@@ -0,0 +1,31 @@
+const check = require("../check");
+
+describe("check", () => {
+  let onSuccess;
+  let onFail;
+
+  beforeEach(() => {
+    onSuccess = jest.fn();
+    onFail = jest.fn();
+  });
+
+  it("should call onSuccess when predicate is true", () => {
+    check(() => true, onSuccess, onFail);
+    // expect(onSuccess.mock.calls.length).toBe(1);
+    expect(onSuccess).toHaveBeenCalledTimes(1);
+    // expect(onSuccess.mock.calls[0][0]).toBe("yes");
+    expect(onSuccess).toHaveBeenCalledWith("yes");
+    // expect(onFail.mock.calls.length).toBe(0);
+    expect(onFail).toHaveBeenCalledTimes(0);
+  });
+
+  // mock을 사용해서 몇번 호출됐는지 무엇과 함께 호출 됐는지 테스트할 수 있으며,
+  // 좀 더 간단한 API를 사용해서 직관적이고 한번에 테스트하는 방법이 있다.
+
+  it("should call onFail when predicate is false", () => {
+    check(() => false, onSuccess, onFail);
+    expect(onFail).toHaveBeenCalledTimes(1);
+    expect(onFail).toHaveBeenCalledWith("no");
+    expect(onSuccess).toHaveBeenCalledTimes(0);
+  });
+});
