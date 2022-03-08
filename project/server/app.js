bb9bf85b7a54091914e977157fa825835f26a5b2
yoo-myeong <yoou201@naver.com>
2022-03-08 21:50:41 +0900

    app.js 통합테스트를 위해 리팩토링

--- project/server/app.js
+++ project/server/app.js
@@ -1,38 +1,57 @@
-import express from 'express'
-import cors from 'cors'
-import morgan from 'morgan'
-import helmet from 'helmet'
-import 'express-async-errors'
-import tweetsRouter from './router/tweets.js'
-import authRouter from './router/auth.js'
-import { config } from './config.js'
-import { sequelize } from './db/database.js'
-import { initSocket } from './connection/socket.js';
+import express from "express";
+import "express-async-errors";
+import cors from "cors";
+import morgan from "morgan";
+import helmet from "helmet";
+import tweetsRouter from "./router/tweets.js";
+import authRouter from "./router/auth.js";
+import { config } from "./config.js";
+import { initSocket, getSocketIO } from "./connection/socket.js";
+import { sequelize } from "./db/database.js";
+import { TweetController } from "./controller/tweets.js";
+import * as tweetRepository from "./data/tweets.js";
 
-const app = express();
 const corsOption = {
-    origin: config.cors.allowedOrigin,
-    optionsSuccessStatus:200,
-}
-app.use(express.json());
-app.use(helmet());
-app.use(cors(corsOption));
-app.use(morgan('tiny'));
+  origin: config.cors.allowedOrigin,
+  optionsSuccessStatus: 200,
+};
 
-app.use('/tweets', tweetsRouter)
-app.use('/auth', authRouter)
-app.use((req,res)=>{
+export async function startServer() {
+  const app = express();
+  app.use(express.json());
+  app.use(helmet());
+  app.use(cors(corsOption));
+  app.use(morgan("tiny"));
+
+  app.use("/tweets", tweetsRouter(new TweetController(tweetRepository, getSocketIO)));
+  app.use("/auth", authRouter);
+
+  app.use((req, res, next) => {
     res.sendStatus(404);
-})
+  });
 
-app.use((error,req,res,next)=>{
+  app.use((error, req, res, next) => {
     console.error(error);
     res.sendStatus(500);
-})
+  });
 
+  await sequelize.sync();
 
-sequelize.sync().then(()=>{
-    console.log('server is started')
-    const server = app.listen(config.port);
-    initSocket(server);
-})
+  console.log("Server is started....");
+  const server = app.listen(config.port);
+  initSocket(server);
+  return server;
+}
+
+export async function stopServer(server) {
+  return new Promise((resolve, reject) => {
+    server.close(async () => {
+      try {
+        await sequelize.close();
+        resolve();
+      } catch (error) {
+        reject(error);
+      }
+    });
+  });
+}
